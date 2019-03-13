5407f3aabc609ee74461c49bec7effcaf0824357
Ruslan Isaev <ubijca16@gmail.com>
2019-03-13 16:39:49 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace GelDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tagsBox = new System.Windows.Forms.TextBox();
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.domainBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // tagsBox
+            // 
+            this.tagsBox.Location = new System.Drawing.Point(74, 45);
+            this.tagsBox.Name = "tagsBox";
+            this.tagsBox.Size = new System.Drawing.Size(320, 20);
+            this.tagsBox.TabIndex = 0;
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Location = new System.Drawing.Point(74, 71);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(75, 23);
+            this.downloadButton.TabIndex = 1;
+            this.downloadButton.Text = "Download";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.downloadButton_Click);
+            // 
+            // domainBox
+            // 
+            this.domainBox.Location = new System.Drawing.Point(74, 19);
+            this.domainBox.Name = "domainBox";
+            this.domainBox.Size = new System.Drawing.Size(100, 20);
+            this.domainBox.TabIndex = 2;
+            this.domainBox.Text = "http://gelbooru.com";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Website:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tags:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(409, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Status:";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.statusLabel.ForeColor = System.Drawing.Color.Green;
+            this.statusLabel.Location = new System.Drawing.Point(482, 25);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(65, 20);
+            this.statusLabel.TabIndex = 6;
+            this.statusLabel.Text = "Ready.";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(155, 75);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(121, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Keep original names";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 442);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.domainBox);
+            this.Controls.Add(this.downloadButton);
+            this.Controls.Add(this.tagsBox);
+            this.Name = "Form1";
+            this.Text = "GelDownloader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tagsBox;
+        private System.Windows.Forms.Button downloadButton;
+        private System.Windows.Forms.TextBox domainBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+
+namespace GelDownloader
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool AllocConsole();
+
+        XmlDocument doc = new XmlDocument();
+        XmlElement root;
+        string url;
+        string tags;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bool exists = System.IO.Directory.Exists("./out/");
+            if(!exists)
+                System.IO.Directory.CreateDirectory("./out/");
+        }
+
+        private int getPostCount()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(domainBox.Text + "/index.php?page=dapi&s=post&q=index&limit=1&tags=" + tagsBox.Text);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            //AllocConsole();
+            string result = "";
+            using (Stream stream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+                String responseString = reader.ReadToEnd();
+                var match = Regex.Match(responseString, "count=\"([\\s\\S]+?)\" ");
+                //var match = Regex.Match(responseString, "file_url=[^ ]+");
+                if (match.Success)
+                {
+                    Console.WriteLine(match.Groups[1].Value + " posts found");
+                    result = match.Groups[1].Value;
+                }
+            }
+            return Convert.ToInt32(result);
+        }
+
+        private string downloadPosts(int page)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(domainBox.Text + "/index.php?page=dapi&s=post&q=index&limit=1&tags=" + tagsBox.Text + "&pid=" + page);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            using (Stream stream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+                String responseString = reader.ReadToEnd();
+
+                doc.LoadXml(responseString);
+                root = doc.DocumentElement;
+                var posts = root.InnerXml;
+
+                doc.LoadXml(posts);
+                root = doc.DocumentElement;
+
+                url = root.Attributes["file_url"].Value;
+                tags = root.Attributes["tags"].Value;
+                downloadImage(url, tags);
+            }
+            return url;
+        }
+        string ExtFromURL(string line)
+        {
+            var ext = "";
+            var match = Regex.Match(line, "(?:)\\.[\\d\\w]+$");
+            if(match.Success)
+                ext = match.Value;
+            return ext;
+        }
+        string FnameFromURL(string line)
+        {
+            var fname = "";
+            var match = Regex.Match(line, "(?:)[\\d\\w]+\\.[\\d\\w]+$");
+            if (match.Success)
+                fname = match.Value;
+            return fname;
+        }
+
+
+        private void downloadImage(string url, string tags)
+        {
+                    using (WebClient wc = new WebClient())
+                    {
+                        if(checkBox1.Checked)
+                            wc.DownloadFileAsync(new System.Uri(url), "./out/" + FnameFromURL(url));//TODO: Files must be downloaded to out/ directory.
+                        else
+                            wc.DownloadFileAsync(new System.Uri(url), "./out/" + tags + ExtFromURL(url));//TODO: Files must be downloaded to out/ directory.
+                    }
+        }
+
+        private void downloadButton_Click(object sender, EventArgs e)
+        {
+
+            int postCount = getPostCount();
+            if (postCount == 0)
+            {
+                Console.WriteLine("No posts found by tag " + tagsBox.Text);
+                statusLabel.ForeColor = Color.Red;
+                statusLabel.Text = "No posts found.";
+            }
+            else
+            {
+                statusLabel.ForeColor = Color.Blue;
+                statusLabel.Text = "Downloading...";
+                for (int i = 0; i < postCount; i++)
+                {
+                    downloadPosts(i);
+                }
+                statusLabel.ForeColor = Color.Green;
+                statusLabel.Text = "Ready.";
+                MessageBox.Show("Download compelete!", "GelDownloader", MessageBoxButtons.OK, MessageBoxIcon.None, MessageBoxDefaultButton.Button1, MessageBoxOptions.ServiceNotification);    
+            }
+        }
+    }
+}
