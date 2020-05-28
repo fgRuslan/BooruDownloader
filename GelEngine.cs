c3fa8490c2cb67f6b4ca541b977e77ccbb57d052
Ruslan Isaev <ubijca16@gmail.com>
2020-05-28 11:35:30 +0300

    Fix downloading of pictures with lots of tags

--- GelEngine.cs
+++ GelEngine.cs
@@ -9,6 +9,7 @@ using System.Runtime.InteropServices;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Xml;
+using System.Windows.Forms;
 
 namespace BooruDownloader
 {
@@ -41,17 +42,43 @@ namespace BooruDownloader
         }
 
 
-        private void downloadImage(string url, string tags, bool keepOriginalNames, string rating)
+        string Truncate(string value, int maxChars)
         {
+            return value.Length <= maxChars ? value : value.Substring(0, maxChars) + "...";
+        }
+
+        private async void downloadImage(string url, string tags, bool keepOriginalNames, string rating)
+        {
+            //I don't know what is this shit!
+            string fullpath = "./out/" + rating + tags + ExtFromURL(url);
+            string regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+            Regex r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
+            fullpath = r.Replace(fullpath, "");
+
+            string shortPath = Path.GetFullPath("./out/" + rating + ExtFromURL(url));
+            string extension = fullpath.Substring(fullpath.Length - 5);
+            if (fullpath.Length > 259)
+                fullpath = Truncate(fullpath, 259 - shortPath.Length - 4);
+            fullpath = fullpath.Substring(5);
+
             using (WebClient wc = new WebClient())
             {
                 wc.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; " +
-                                  "Windows NT 5.2; .NET CLR 1.0.3705;)");
-                if (keepOriginalNames)
-                    wc.DownloadFileAsync(new System.Uri(url), "./out/"+ rating + FnameFromURL(url));
-                else
-                    wc.DownloadFileAsync(new System.Uri(url), "./out/"+ rating + tags + ExtFromURL(url));
-            }
+                                      "Windows NT 5.2; .NET CLR 1.0.3705;)");
+                try
+                {
+                    if (keepOriginalNames)
+                        wc.DownloadFileAsync(new System.Uri(url), "./out/" + rating + FnameFromURL(url));
+                    else
+                        await wc.DownloadFileTaskAsync(new System.Uri(url), "./out/" + fullpath + extension);
+                }
+                catch (Exception ex)
+                {
+                    //MessageBox.Show(ex.ToString());
+                    MessageBox.Show(ex.GetType().ToString());
+                    wc.DownloadFileAsync(new System.Uri(url), "./out/" + rating + FnameFromURL(url));
+                }
+            }          
         }
 
         public int getPostCount(string domain, string tags)
