f7d6038b4de8e41e70b83f75a81d33222f6c4b38
Chay <13159328+ShujianDou@users.noreply.github.com>
2021-08-19 08:18:54 -0400

    Simplification of Dectector (#4)
    * Better Detector
    
    Dunno if it works, since I haven't tested it, but I believe this is a much better way of doing this.
    
    * add ';'
    
    * Update Detector.cs

--- Detector.cs
+++ Detector.cs
@@ -6,30 +6,56 @@ using System.Threading.Tasks;
 
 namespace BooruDownloader
 {
+    public abstract class engineBase
+    {
+        List<string> hostsContainer = new List<string>();
+        public string host { get { return hostsContainer[0]; } set { hostsContainer.Add(value); }  }
+        public abstract EngineBase GenEngine();
+
+        public bool chkHost(string host)
+            => hostsContainer.Contains(host);
+    }
+
+    public class danEngine : engineBase
+    {
+        public danEngine() { host = "danbooru.donmai.us";  host = "danbooru.donmai.us"; }
+
+        public override EngineBase GenEngine()
+            => new DanEngine();
+    }
+    public class gelEngine : engineBase
+    {
+        public gelEngine() { host = "rule34.xxx"; host = "safebooru.org"; host = "gelbooru.org"; }
+
+        public override EngineBase GenEngine()
+            => new GelEngine();
+    }
     class Detector
     {
-
-        static Dictionary<string, string> engines = new Dictionary<string, string>
+        public static engineBase[] engines = new engineBase[] { new gelEngine(), new danEngine() };
+        public static EngineBase detectEngine(String url)
+            => engines.First(x => x.chkHost(gHost(url))).GenEngine();
+        
+        private string gHost(string uri)
         {
-            {"safebooru.org", "Gel"},
-            {"safebooru.donmai.us", "Dan"},
-            {"danbooru.donmai.us", "Dan"},
-            {"gelbooru.org", "Gel"},
-            {"rule34.xxx", "Gel"}
-        };
+            for (int idx = 0; idx < uri.Length; idx++)
+                if (char.Equals(uri[idx], '/') && char.Equals(uri[idx + 1], '/'))
+                    return LoopUntilSlash(uri, idx + 2);
+            return LoopUntilSlash(uri, 0);
+        }
 
-        public static String detectEngine(String url)
+        private string LoopUntilSlash(string l, int idx)
         {
-            if (url.StartsWith("http://"))
-                url = url.Substring(7);
-            if (url.StartsWith("https://"))
-                url = url.Substring(8);
-            foreach (var pair in engines)
+            StringBuilder builder = new StringBuilder();
+            builder.Capacity = l.Length;
+            for (int i = idx; i < l.Length; i++)
             {
-                if(url.StartsWith(pair.Key))
-                    return pair.Value;
+                if (!char.Equals(l[i], '/'))
+                    builder.Append(l[i]);
+                else
+                    return builder.ToString();
             }
-            return "";
+            return builder.ToString();
         }
     }
 }
---
+++ EngineBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace BooruDownloader
+{
+    public abstract class EngineBase
+    {
+        public XmlDocument doc = new XmlDocument();
+        public XmlElement root;
+        public string url;
+        public string rating;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool AllocConsole();
+
+        public abstract string ExtFromURL(string line);
+        public abstract string FnameFromURL(string line);
+        public abstract string Truncate(string line, int mChar);
+        public abstract void downloadImage(string url, string tags, bool keepOrigName, string rating);
+        public abstract string downloadPosts(string url, string tags, int page, bool keepOrigName, bool inclRating);
+        public abstract int getPostCount(string domain, string tags);
+    }
+}
